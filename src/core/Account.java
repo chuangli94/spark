b4bdcff8f6432a9638803898a2fa347e998e1480
James <f7jiang@uwaterloo.ca>
2015-06-20 16:29:28 -0400

    fixed some classpath issues

--- src/core/Account.java
+++ src/core/Account.java
@@ -1,4 +1,4 @@
-package main.java.core;
+package core;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
--- src/core/Application.java
+++ src/core/Application.java
@@ -1,4 +1,4 @@
-package main.java.core;
+package core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
